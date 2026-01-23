47e24b04a4eb0852ff5b40aa16734f644e31c054
Ama114 <amathun340@gmail.com>
2026-01-23 16:21:59 +0530

    Update experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -56,7 +56,7 @@ export default function Experience() {
           }`}
         >
           <div className="text-center mb-16">
-            <h2 className="text-2xl md:text-4xl lg:text-5xl font-bold mb-6">
+            <h2 className="text-xl md:text-2xl lg:text-3xl font-bold mb-6">
               <span className="text-foreground font-mono text-xl md:text-2xl">02.</span>{" "}
               <span className="text-foreground">Education & Experience</span>
             </h2>
